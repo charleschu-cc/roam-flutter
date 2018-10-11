3cf50b9fc08a4fff0d9728a32e3dec83820dec8b
charleschu <charleschu.cc@gmail.com>
2018-10-11 22:26:37 +0800

    Add favourite feature

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-import 'placeholder_widget.dart';
 import 'list_widget.dart';
+import 'suggestion_widget.dart';
 
 
 class Home extends StatefulWidget {
@@ -16,23 +16,20 @@ class HomeState extends State<Home> {
 
   int _currentNavIndex = 0;
 
+  SuggestionWidget suggest;
   ListWidget list;
   ProfileWidget profile;
   List<Widget> _navChildren;
 
-  // final List<Widget> _navChildren = [
-  //   ListWidget(Colors.white),
-  //   EditWidget(Colors.black),
-  //   PlaceholderWidget(Colors.grey)
-  // ];
 
   @override
   void initState() {
     list = ListWidget();
     profile = ProfileWidget(Colors.white);
+    suggest = SuggestionWidget(_saved);
 
     _navChildren = [
-      _buildSuggestions(),
+      suggest,
       list,
       profile
     ];
@@ -53,7 +50,6 @@ class HomeState extends State<Home> {
           new IconButton(icon: const Icon(Icons.tune), onPressed: _pushSaved),
         ],
       ),
-      //body: _buildSuggestions(),
       body: _navChildren[_currentNavIndex],
       drawer: Drawer (
         child: Column(
@@ -104,82 +100,6 @@ class HomeState extends State<Home> {
     );
   }
 
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        //if (i.isOdd) return Divider();
-
-        final index = i ~/ 2;
-
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      }
-    );
-  }
-
-  // Widget _buildRow(WordPair pair) {
-  //   final bool alreadySaved = _saved.contains(pair);
-  //   return ListTile(
-  //     title: Text(
-  //       pair.asPascalCase,
-  //       style: _biggerFont,
-  //     ),
-  //     trailing: new Icon(
-  //       alreadySaved ? Icons.favorite : Icons.favorite_border,
-  //       color: alreadySaved ? Colors.red : null,
-  //     ),
-  //     onTap: () {
-  //       setState(() {
-  //         //alreadySaved = !alreadySaved;
-  //         if (alreadySaved) {
-  //           _saved.remove(pair);
-  //         } else {
-  //           _saved.add(pair);
-  //         }
-  //       });
-  //     },
-  //   );
-  // }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return Card(
-      child: new Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Image.asset(
-            'images/lake.jpg',
-            width: 600.0,
-            height: 240.0,
-            fit: BoxFit.cover,
-          ),
-          const ListTile(
-            leading: const Icon(Icons.album),
-            title: const Text('The Enchanted Nightingale'),
-            subtitle: const Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-          ),
-          new ButtonTheme.bar(
-            child: new ButtonBar(
-              children: <Widget>[
-                new FlatButton(
-                  child: const Text('Buy Tickets'),
-                  onPressed: () {   },
-                ),
-                new FlatButton(
-                  child: const Text('LISTEN'),
-                  onPressed: () {  },
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   void _pushSaved() {
     Navigator.of(context).push(
       new MaterialPageRoute<void>(
@@ -210,7 +130,6 @@ class HomeState extends State<Home> {
         },
       ),
     );
-
   }
 
   void _addAccount(BuildContext context) {
--- lib/suggestion_widget.dart
+++ lib/suggestion_widget.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-class SuggestionWidget extends StatelessWidget {
+class SuggestionWidget extends StatefulWidget {
+  final Set<WordPair> saved;
+
+  SuggestionWidget(this.saved);
+
+  @override
+  SuggestionState createState() => new SuggestionState();
+}
+
+class SuggestionState extends State<SuggestionWidget> {
   final _suggestions = <WordPair>[];
   final Set<WordPair> _saved = new Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
@@ -18,7 +27,7 @@ class SuggestionWidget extends StatelessWidget {
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
-        return _buildRow(_suggestions[index]);
+        return _buildCard(_suggestions[index]);
       }
     );
   }
@@ -43,6 +52,59 @@ class SuggestionWidget extends StatelessWidget {
           }
         });
       },
+    );
+  }
+
+  Widget _buildCard(WordPair pair) {
+    final bool alreadySaved = widget.saved.contains(pair);
+    //print(pair.asPascalCase);
+    return Card(
+      child: new Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Image.asset(
+            'images/lake.jpg',
+            width: 600.0,
+            height: 240.0,
+            fit: BoxFit.cover,
+          ),
+          new ListTile(
+            leading: const Icon(Icons.album),
+            title: new Text(pair.asPascalCase),
+            //title: const Text('The Enchanted Nightingale'),
+            subtitle: const Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+          ),
+          new ButtonTheme.bar(
+            child: new ButtonBar(
+              children: <Widget>[
+                new FlatButton(
+                  child: const Text('Buy Tickets'),
+                  onPressed: () {   },
+                ),
+                new FlatButton(
+                  child: const Text('LISTEN'),
+                  onPressed: () {  },
+                ),
+                new IconButton(
+                  icon: (alreadySaved
+                      ? Icon(Icons.favorite)
+                      : Icon(Icons.favorite_border)),
+                  color: alreadySaved ? Colors.red : null,
+                  onPressed: () {
+                    setState(() {
+                      if (alreadySaved) {
+                        widget.saved.remove(pair);
+                      } else {
+                        widget.saved.add(pair);
+                      }
+                    });
+                  }
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
